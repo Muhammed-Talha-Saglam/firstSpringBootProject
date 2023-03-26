c8b8b44562af8ccb6140f85b5c97b24a7a8cf697
talha.saglam <1699.mTs>
2023-03-26 15:14:30 +0300

    Exposed and excluded actuator endpoints. Defined custom properties.

--- src/main/kotlin/dev/bytecode/springboot/firstProject/firstProject/rest/FunRest.kt
+++ src/main/kotlin/dev/bytecode/springboot/firstProject/firstProject/rest/FunRest.kt
@@ -1,13 +1,27 @@
 package dev.bytecode.springboot.firstProject.firstProject.rest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class FunRest {
 
+    @Value("\${team.name}")
+    private lateinit var teamName: String
+
     @GetMapping("/")
     fun home(): String {
-        return "Spring is coming..."
+        return "Spring is coming by $teamName"
+    }
+
+    @GetMapping("/workout")
+    fun dailyWorkout(): String {
+        return "Run a hard 5k!"
+    }
+
+    @GetMapping("/fortune")
+    fun dailyFortune(): String {
+        return "Today is your lucky day."
     }
 }
