3373111e7a17db69e9666666419ab5771e466409
talha.saglam <1699.mTs>
2023-04-02 16:35:54 +0300

    Added Exception Handlers. Added EmployeeRepository with JpaRepository extension. Added Data REST controller dependency.

--- src/main/kotlin/dev/bytecode/springboot/firstProject/firstProject/rest/EmployeeRestController.kt
+++ src/main/kotlin/dev/bytecode/springboot/firstProject/firstProject/rest/EmployeeRestController.kt
@@ -13,40 +13,39 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.lang.RuntimeException
 
-@RestController
-@RequestMapping("/api")
-class EmployeeRestController @Autowired constructor (
-    private val employeeService: EmployeeService
-) {
-
-    @GetMapping("/employees")
-    fun findAll(): List<Employee> {
-        return employeeService.findAll()
-    }
-
-    @GetMapping("/employees/{employeeId}")
-    fun getEmployee(@PathVariable employeeId: Int): Employee? {
-        return employeeService.findById(employeeId) ?: throw RuntimeException("Employee Id not found - $employeeId")
-    }
-
-    @PostMapping("/employees")
-    fun addEmployee(@RequestBody employee: Employee): Employee? {
-       return employeeService.save(employee.apply { id = 0 })
-    }
-
-    @PutMapping("/employees")
-    fun updateEmployee(@RequestBody employee: Employee): Employee? {
-        return employeeService.save(employee)
-    }
-
-    @DeleteMapping("/employees/{employeeId}")
-    fun deleteEmployee(@PathVariable employeeId: Int): String {
-        val employee = employeeService.findById(employeeId) ?: throw RuntimeException("Employee Id not found - $employeeId")
-
-        employeeService.deleteById(employeeId)
-
-        return "Deleted employee - $employeeId"
-    }
-
-
-}
+//@RestController
+//@RequestMapping("/api")
+//class EmployeeRestController @Autowired constructor (
+//    private val employeeService: EmployeeService
+//) {
+//
+//    @GetMapping("/employees")
+//    fun findAll(): List<Employee> {
+//        return employeeService.findAll()
+//    }
+//
+//    @GetMapping("/employees/{employeeId}")
+//    fun getEmployee(@PathVariable employeeId: Int): Employee? {
+//        return employeeService.findById(employeeId) ?: throw RuntimeException("Employee Id not found - $employeeId")
+//    }
+//
+//    @PostMapping("/employees")
+//    fun addEmployee(@RequestBody employee: Employee): Employee? {
+//       return employeeService.save(employee.apply { id = 0 })
+//    }
+//
+//    @PutMapping("/employees")
+//    fun updateEmployee(@RequestBody employee: Employee): Employee? {
+//        return employeeService.save(employee)
+//    }
+//
+//    @DeleteMapping("/employees/{employeeId}")
+//    fun deleteEmployee(@PathVariable employeeId: Int): String {
+//        val employee = employeeService.findById(employeeId) ?: throw RuntimeException("Employee Id not found - $employeeId")
+//
+//        employeeService.deleteById(employeeId)
+//
+//        return "Deleted employee - $employeeId"
+//    }
+//
+//}
